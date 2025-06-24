5545dc75604284e3bd00b2e270ff6c2f6d5cff44
tangxy1024 <tangxiangyan@tangxiangyandeMac-mini.local>
2025-06-24 09:40:55 +0800

    feature:Capture the standard output

--- src/model/components/gxl_flow/flow.rs
+++ src/model/components/gxl_flow/flow.rs
@@ -15,9 +15,12 @@ use crate::traits::DependTrait;
 use crate::components::gxl_block::BlockNode;
 use crate::util::http_handle::send_http_request;
 use std::sync::Arc;
+
+use std::io::Read;
 use std::io::Write;
 
 use derive_getters::Getters;
+use gag::BufferRedirect;
 
 use super::anno::FlowAnnFunc;
 use super::meta::FlowMeta;
@@ -150,10 +153,20 @@ impl GxlFlow {
 
         // 执行块
         for item in &self.blocks {
+            // 重定向日志输出
+            let mut buf = BufferRedirect::stdout().map_err(|e| {
+                ExecError::from(ExecReason::Io(e.to_string()))
+            })?;
             let (cur_dict, out) = item
                 .async_exec_with_dryrun(ctx.clone(), var_dict, self.is_dryrun())
                 .await?;
             var_dict = cur_dict;
+            let mut output = String::new();
+            buf.read_to_string(&mut output).unwrap();
+            // 恢复日志输出
+            buf.into_inner();
+            task.stdout = output.clone();
+            println!("{}", output);
             task.append(out);
         }
         task.finish();
