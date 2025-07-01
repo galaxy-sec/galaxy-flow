eba6d047d4d4ae48fee8cc329e94a286940a8839
wukong <sec-wukong@outlook.com>
2025-07-01 10:27:54 +0800

    add example_tests

--- tests/example_test.rs
+++ tests/example_test.rs
@@ -1,8 +1,9 @@
 //extern crate galaxy_flow;
 //#[cfg(feature = "res_depend_test")]
 mod tests {
+    use galaxy_flow::err::report_gxl_error;
     use galaxy_flow::execution::VarSpace;
-    use galaxy_flow::util::path::WorkDir;
+    use galaxy_flow::util::path::{WorkDir, WorkDirWithLock};
     use galaxy_flow::{
         components::gxl_spc::GxlSpace, err::RunResult, expect::ShellOption, infra::once_init_log,
         GxLoader,
@@ -37,10 +38,11 @@ mod tests {
         }
     }
 
-    #[tokio::test]
+    #[ignore = "reason"]
+    #[tokio::test(flavor = "current_thread")]
     async fn example_read() -> RunResult<()> {
-        once_init_log();
-        let _dir = ScopedRunDir::new("./examples/read");
+        //once_init_log();
+        let _dir = WorkDirWithLock::change("./examples/read");
         let vars = VarSpace::sys_init().assert();
         let mut loader = GxLoader::new();
         let spc =
@@ -57,10 +59,11 @@ mod tests {
         Ok(())
     }
 
+    #[ignore = "reason"]
     #[tokio::test(flavor = "current_thread")]
     async fn example_assert() -> RunResult<()> {
-        once_init_log();
-        let _dir = WorkDir::change("./examples/assert");
+        //once_init_log();
+        let _dir = WorkDirWithLock::change("./examples/assert");
         let vars = VarSpace::sys_init().assert();
         let mut loader = GxLoader::new();
         let spc =
@@ -76,10 +79,11 @@ mod tests {
         .await?;
         Ok(())
     }
+    #[ignore = "reason"]
     #[tokio::test(flavor = "current_thread")]
     async fn example_template() -> RunResult<()> {
-        once_init_log();
-        let _dir = WorkDir::change("./examples/template");
+        //jonce_init_log();
+        let _dir = WorkDirWithLock::change("./examples/template");
         let mut loader = GxLoader::new();
         let vars = VarSpace::sys_init().assert();
         let spc =
@@ -93,6 +97,69 @@ mod tests {
             VarSpace::default(),
         )
         .await?;
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "current_thread")]
+    async fn example_translate() -> RunResult<()> {
+        once_init_log();
+        let _dir = WorkDirWithLock::change("./examples/transaction");
+        let mut loader = GxLoader::new();
+        let vars = VarSpace::sys_init().assert();
+        let spc =
+            GxlSpace::try_from(loader.parse_file("./_gal/work.gxl", false, test_opt(), &vars)?)
+                .assert();
+        let result = spc
+            .exec(
+                vec!["default".into()],
+                vec!["trans1".into()],
+                Some(false),
+                false,
+                VarSpace::default(),
+            )
+            .await;
+        match result {
+            Ok(_) => {
+                panic!("need fail!");
+            }
+            Err(e) => {
+                report_gxl_error(e);
+            }
+        }
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "current_thread")]
+    async fn example_dryrun() -> RunResult<()> {
+        once_init_log();
+        let _dir = WorkDirWithLock::change("./examples/dryrun");
+        let mut loader = GxLoader::new();
+        let vars = VarSpace::sys_init().assert();
+        let spc =
+            GxlSpace::try_from(loader.parse_file("./_gal/work.gxl", false, test_opt(), &vars)?)
+                .assert();
+        let dryrun = true;
+        let _ = spc
+            .exec(
+                vec!["default".into()],
+                vec!["start".into()],
+                Some(false),
+                dryrun,
+                VarSpace::default(),
+            )
+            .await?;
+
+        let dryrun = false;
+        let fail = spc
+            .exec(
+                vec!["default".into()],
+                vec!["start".into()],
+                Some(false),
+                dryrun,
+                VarSpace::default(),
+            )
+            .await;
+        assert!(fail.is_err());
         Ok(())
     }
 }
