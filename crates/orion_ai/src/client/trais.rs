b44b8194c03945b1bf2ef0f45fe0c22cd6b6aa8d
wukong <sec-wukong@outlook.com>
2025-08-16 09:06:25 +0800

    add miss file

---
+++ crates/orion_ai/src/client/trais.rs
@@ -0,0 +1,33 @@
+use crate::capabilities::AiRole;
+use crate::error::AiResult;
+use crate::provider::{AiRequest, AiResponse};
+use async_trait::async_trait;
+
+use super::core::AiClient;
+
+/// AI客户端发送类型枚举
+pub enum AiCoreClient {
+    Basic(AiClient),
+}
+
+/// AI客户端trait定义
+#[async_trait]
+pub trait AiClientTrait: Send + Sync {
+    async fn send_request(&self, request: AiRequest) -> AiResult<AiResponse>;
+    async fn smart_role_request(&self, role: AiRole, user_input: &str) -> AiResult<AiResponse>;
+}
+
+#[async_trait]
+impl AiClientTrait for AiCoreClient {
+    async fn send_request(&self, request: AiRequest) -> AiResult<AiResponse> {
+        match self {
+            Self::Basic(o) => o.send_request(request).await,
+        }
+    }
+
+    async fn smart_role_request(&self, role: AiRole, user_input: &str) -> AiResult<AiResponse> {
+        match self {
+            Self::Basic(o) => o.smart_role_request(role, user_input).await,
+        }
+    }
+}
