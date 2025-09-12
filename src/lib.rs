8aef99e30d3aab2856cc83a7ae0bd8708c17d2f6
wukong <sec-wukong@outlook.com>
2025-09-12 08:47:16 +0800

    Remove unused dependencies and clean up Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,9 @@ extern crate handlebars;
 extern crate serde_json;
 #[macro_use]
 extern crate lazy_static;
-extern crate http_types;
+//extern crate http_types;
 
-extern crate ini;
+//extern crate ini;
 extern crate url;
 
 //#[macro_use]
